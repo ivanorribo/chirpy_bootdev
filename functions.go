4bf3ef01cc52fe609f93439e015b127e7253b9ef
ivanorribo <ivanorribo@gmail.com>
2026-06-20 11:47:07 +0100

    added more handlers, a redirect and changed handlers to only admit one type of request

---
+++ functions.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getFileserverHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) resetFileserverHits(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(200)
+}
--- main.go
+++ main.go
@@ -3,14 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+	handlerCfg     http.Handler
+}
 
 func main() {
 	const port = ":8080"
 	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
 	handler := http.FileServer(http.Dir("."))
 
-	mux.Handle("/", handler)
+	mux.HandleFunc("GET /healthz", handlerReadiness) // add readiness handler using function from functions.go
+	mux.HandleFunc("GET /metrics", apiCfg.getFileserverHits)
+	mux.HandleFunc("POST /reset", apiCfg.resetFileserverHits)
+
+	stripHandler := http.StripPrefix("/app/", handler) // strip the /app prefix so we can differentiate between the /app/ path and the root path
+	mux.Handle("/app", http.RedirectHandler("/app/", http.StatusPermanentRedirect))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(stripHandler))
 
 	Server := &http.Server{
 		Addr:    port,
